7587faafb2ec83e046f5f1b53caf4d3fce2cb867
Mishraji8429 <ashukuma222@gmail.com>
2026-06-29 18:27:09 +0530

    Add function to send WhatsApp message

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-// KSB Social Media Growth Scripts
+function sendWhatsApp(){
+
+let username=document.getElementById("username").value;
+
+let service=document.getElementById("service").value;
+
+let quantity=document.getElementById("quantity").value;
+
+let message=`Hello KSB Social Media Growth 👋
+
+Instagram Username : ${username}
+
+Service : ${service}
+
+Quantity : ${quantity}
+
+Please send me details.`;
+
+window.open(`https://wa.me/916394723648?text=${encodeURIComponent(message)}`);
+
+}
